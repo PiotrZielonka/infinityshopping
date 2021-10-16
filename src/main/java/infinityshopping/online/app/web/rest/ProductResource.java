76bdd6db80626e95480114b7fb66141d5cafe43d
Piotr Zielonka <piotr.zielonka@hotmail.com>
2021-10-16 20:33:02 +0200

    checkstyle test

--- src/main/java/infinityshopping/online/app/web/rest/ProductResource.java
+++ src/main/java/infinityshopping/online/app/web/rest/ProductResource.java
@@ -44,11 +44,8 @@ public class ProductResource {
 
     private final ProductService productService;
 
-    private final ProductRepository productRepository;
-
-    public ProductResource(ProductService productService, ProductRepository productRepository) {
+    public ProductResource(ProductService productService) {
         this.productService = productService;
-        this.productRepository = productRepository;
     }
 
     @PostMapping("/products")
@@ -64,23 +61,12 @@ public class ProductResource {
             .body(result);
     }
 
-    @PutMapping("/products/{id}")
-    public ResponseEntity<ProductDTO> updateProduct(
-        @PathVariable(value = "id", required = false) final Long id,
-        @Valid @RequestBody ProductDTO productDto
-    ) throws URISyntaxException {
-        log.debug("REST request to update Product : {}, {}", id, productDto);
+    @PutMapping("/products")
+    public ResponseEntity<ProductDTO> updateProduct(@Valid @RequestBody ProductDTO productDto) throws URISyntaxException {
+        log.debug("REST request to update Product : {}", productDto);
         if (productDto.getId() == null) {
             throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
         }
-        if (!Objects.equals(id, productDto.getId())) {
-            throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
-        }
-
-        if (!productRepository.existsById(id)) {
-            throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
-        }
-
         ProductDTO result = productService.save(productDto);
         return ResponseEntity
             .ok()
@@ -90,16 +76,48 @@ public class ProductResource {
 
     @GetMapping("/products/all")
     public ResponseEntity<List<ProductDTO>> getAllProducts(Pageable pageable) {
-        log.debug("REST request to get a page of Products");
+        log.debug("REST request to get all Products");
         Page<ProductDTO> page = productService.findAll(pageable);
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
         return ResponseEntity.ok().headers(headers).body(page.getContent());
     }
 
-    @GetMapping("/products/all/imageNamePriceGross")
-    public ResponseEntity<List<ProductDtoImageNamePriceGross>> getAllProductsOnlyWithImageNamePriceGross(Pageable pageable) {
-        log.debug("Request to get all Products only with Image Name PriceGross");
-        Page<ProductDtoImageNamePriceGross> page = productService.findAllImageNamePriceGross(pageable);
+    @GetMapping("/products/category/probiotics")
+    public ResponseEntity<List<ProductDTO>> getAllProductsByCategoryProbiotics(Pageable pageable) {
+        log.debug("REST request to get all Products by category probiotics");
+        Page<ProductDTO> page = productService.getAllProductsByCategoryProbiotics(pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
+
+    @GetMapping("/products/category/vitamins")
+    public ResponseEntity<List<ProductDTO>> getAllProductsByCategoryVitamins(Pageable pageable) {
+        log.debug("REST request to get all Products by category probiotics");
+        Page<ProductDTO> page = productService.getAllProductsByCategoryVitamins(pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
+
+    @GetMapping("/products/category/minerals")
+    public ResponseEntity<List<ProductDTO>> getAllProductsByCategoryMinerals(Pageable pageable) {
+        log.debug("REST request to get all Products by category minerals");
+        Page<ProductDTO> page = productService.getAllProductsByCategoryMinerals(pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
+
+    @GetMapping("/products/category/aloes")
+    public ResponseEntity<List<ProductDTO>> getAllProductsByCategoryAloes(Pageable pageable) {
+        log.debug("REST request to get all Products by category aloes");
+        Page<ProductDTO> page = productService.getAllProductsByCategoryAloes(pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
+
+    @GetMapping("/products/category/collagen")
+    public ResponseEntity<List<ProductDTO>> getAllProductsByCategoryCollagen(Pageable pageable) {
+        log.debug("REST request to get all Products by category collagen");
+        Page<ProductDTO> page = productService.getAllProductsByCategoryCollagen(pageable);
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
         return ResponseEntity.ok().headers(headers).body(page.getContent());
     }
